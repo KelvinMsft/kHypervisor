24fa3830546651c99052c2cea5c60beed7c842ad
KelvinChan <kelvin1272011@gmail.com>
2017-01-20 20:44:32 +0800

    updated

--- kHypervisor/HyperPlatform/vmm.cpp
+++ kHypervisor/HyperPlatform/vmm.cpp
@@ -784,7 +784,7 @@ _Use_decl_annotations_ static void VmmpHandleException(
       const auto fault_address = UtilVmRead(VmcsField::kExitQualification);
  
 	  VmmpInjectInterruption(interruption_type, vector, exception.fields.error_code_valid, fault_code.all);
-	  if (KdDebuggerNotPresent)
+	  if (!KdDebuggerNotPresent)
 	  { 
 			  HYPERPLATFORM_LOG_INFO(" | kGuestCr3: %I64X |  Gs: %x  |  GuestGsBase: %I64x  | Msr_gs_base: %I64X | Msr_kernel_gs_base: %I64X | fault_code: %I64X  | fault_address: %I64X | RIP Address: %I64X | \r\n",
 				  UtilVmRead(VmcsField::kGuestCr3),

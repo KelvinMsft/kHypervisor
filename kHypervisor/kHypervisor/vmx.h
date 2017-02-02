8ff314f38d0abedd7e30677c51537175053e5d82
Kelvinhack <kelvin1272011@gmail.com>
2017-02-03 05:46:44 +0800

    Add files via upload

---
+++ kHypervisor/kHypervisor/vmx.h
@@ -0,0 +1,62 @@
+// Copyright (c) 2016-2017, KelvinChan. All rights reserved.
+// Copyright (c) 2016-2017, KelvinChan. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+#ifndef NESTED_HYPERPLATFORM_VMX_H_
+#define NESTED_HYPERPLATFORM_VMX_H_
+#include <fltKernel.h>
+#include "..\HyperPlatform\vmm.h"
+extern struct GuestContext;
+extern "C"
+{
+
+VOID VmxonEmulate(
+	GuestContext* guest_context
+);
+
+VOID VmclearEmulate(
+	GuestContext* guest_context
+);
+
+VOID VmptrldEmulate(
+	GuestContext* guest_context
+);
+
+VOID VmreadEmulate(
+	GuestContext* guest_context
+);
+
+VOID VmwriteEmulate(
+	GuestContext* guest_context
+);
+
+VOID VmlaunchEmulate(
+	GuestContext* guest_context
+);
+
+VOID VmresumeEmulate(
+	GuestContext* guest_context
+);
+
+VOID VmptrstEmulate(
+	GuestContext* guest_context
+);
+
+
+VOID LEAVE_GUEST_MODE(
+	NestedVmm* vcpu
+);
+VOID ENTER_GUEST_MODE(
+	NestedVmm* vcpu
+);
+BOOLEAN IsRootMode(
+	NestedVmm* vcpu
+);  
+void vmx_save_guest_msrs(
+	NestedVmm* vcpu
+);
+
+}
+
+#endif

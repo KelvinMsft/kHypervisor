9e5cd2862b2dd1612f95286ba488a0cd68a7dd8a
KelvinChan <kelvin1272011@gmail.com>
2017-02-08 15:05:43 +0800

    Fixed bug - upload bsod

--- NestedHypervisor/HyperPlatform/HyperPlatform/driver.cpp
+++ NestedHypervisor/HyperPlatform/HyperPlatform/driver.cpp
@@ -36,31 +36,31 @@ typedef struct _DEVICE_EXTENSION
 
 
 extern "C" {
-////////////////////////////////////////////////////////////////////////////////
-//
-// macro utilities
-//
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	// macro utilities
+	//
 
-////////////////////////////////////////////////////////////////////////////////
-//
-// constants and macros
-//
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	// constants and macros
+	//
 
-////////////////////////////////////////////////////////////////////////////////
-//
-// types
-//
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	// types
+	//
 
-////////////////////////////////////////////////////////////////////////////////
-//
-// prototypes
-//
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	// prototypes
+	//
 
-DRIVER_INITIALIZE DriverEntry;
+	DRIVER_INITIALIZE DriverEntry;
 
-static DRIVER_UNLOAD DriverpDriverUnload;
+	static DRIVER_UNLOAD DriverpDriverUnload;
 
-_IRQL_requires_max_(PASSIVE_LEVEL) bool DriverpIsSuppoetedOS();
+	_IRQL_requires_max_(PASSIVE_LEVEL) bool DriverpIsSuppoetedOS();
 
 #if defined(ALLOC_PRAGMA)
 #pragma alloc_text(INIT, DriverEntry)
@@ -68,196 +68,197 @@ _IRQL_requires_max_(PASSIVE_LEVEL) bool DriverpIsSuppoetedOS();
 #pragma alloc_text(INIT, DriverpIsSuppoetedOS)
 #endif
 
-////////////////////////////////////////////////////////////////////////////////
-//
-// variables
-//
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	// variables
+	//
 #define IOCTL_TRANSFER_TYPE( _iocontrol)   (_iocontrol & 0x3)
 
-////////////////////////////////////////////////////////////////////////////////
-//
-// implementations
-// 
-NTSTATUS DDI_devCtrlRoutine(
-	IN PDEVICE_OBJECT		DeviceObject,
-	IN PIRP					Irp
-)
-{ 
-	PIO_STATUS_BLOCK	ioStatus;								 
-	PIO_STACK_LOCATION	pIrpStack;								 
-	PDEVICE_EXTENSION	deviceExtension;						 
-	PVOID				inputBuffer, outputBuffer;				 
-	ULONG				inputBufferLength, outputBufferLength;	 
-	ULONG				ioControlCode;							 
-
-	/*	dprintf("[$XTR]--->IrpMjXTRdevCtrlRoutine( 0x%08x, 0x%08x ).\n", DeviceObject, Irp);*/
-
-	pIrpStack = IoGetCurrentIrpStackLocation(Irp);
-	deviceExtension = (PDEVICE_EXTENSION)DeviceObject->DeviceExtension;
-
-	ioStatus = &Irp->IoStatus;
-	ioStatus->Status = STATUS_SUCCESS;	// Assume success
-	ioStatus->Information = 0;              // Assume nothing returned
-
-											//
-											// Get the pointer to the input/output buffer and it's length
-	inputBuffer = Irp->AssociatedIrp.SystemBuffer;
-	inputBufferLength = pIrpStack->Parameters.DeviceIoControl.InputBufferLength;
-	outputBuffer = Irp->AssociatedIrp.SystemBuffer;
-	outputBufferLength = pIrpStack->Parameters.DeviceIoControl.OutputBufferLength;
-	ioControlCode = pIrpStack->Parameters.DeviceIoControl.IoControlCode;
-
-
-	switch (pIrpStack->MajorFunction)
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	// implementations
+	// 
+	NTSTATUS DDI_devCtrlRoutine(
+		IN PDEVICE_OBJECT		DeviceObject,
+		IN PIRP					Irp
+	)
 	{
-	case IRP_MJ_CREATE:
-		DbgPrint("[$ARK]<-IRP_MJ_CREATE.\n");
-		break;
+		PIO_STATUS_BLOCK	ioStatus;
+		PIO_STACK_LOCATION	pIrpStack;
+		PDEVICE_EXTENSION	deviceExtension;
+		PVOID				inputBuffer, outputBuffer;
+		ULONG				inputBufferLength, outputBufferLength;
+		ULONG				ioControlCode;
 
-	case IRP_MJ_CLOSE:
-		DbgPrint("[$ARK]->IRP_MJ_CLOSE.\n");
-		break;
+		/*	dprintf("[$XTR]--->IrpMjXTRdevCtrlRoutine( 0x%08x, 0x%08x ).\n", DeviceObject, Irp);*/
 
-	case IRP_MJ_SHUTDOWN:
-		DbgPrint("[$ARK] IRP_MJ_SHUTDOWN.\n");
-		break;
+		pIrpStack = IoGetCurrentIrpStackLocation(Irp);
+		deviceExtension = (PDEVICE_EXTENSION)DeviceObject->DeviceExtension;
 
-	case IRP_MJ_DEVICE_CONTROL:
-		if (IOCTL_TRANSFER_TYPE(ioControlCode) == METHOD_NEITHER)
+		ioStatus = &Irp->IoStatus;
+		ioStatus->Status = STATUS_SUCCESS;	// Assume success
+		ioStatus->Information = 0;              // Assume nothing returned
+
+												//
+												// Get the pointer to the input/output buffer and it's length
+		inputBuffer = Irp->AssociatedIrp.SystemBuffer;
+		inputBufferLength = pIrpStack->Parameters.DeviceIoControl.InputBufferLength;
+		outputBuffer = Irp->AssociatedIrp.SystemBuffer;
+		outputBufferLength = pIrpStack->Parameters.DeviceIoControl.OutputBufferLength;
+		ioControlCode = pIrpStack->Parameters.DeviceIoControl.IoControlCode;
+
+
+		switch (pIrpStack->MajorFunction)
 		{
-			DbgPrint("[$ARK] METHOD_NEITHER\n");
-			outputBuffer = Irp->UserBuffer;
+		case IRP_MJ_CREATE:
+			DbgPrint("[$ARK]<-IRP_MJ_CREATE.\n");
+			break;
+
+		case IRP_MJ_CLOSE:
+			DbgPrint("[$ARK]->IRP_MJ_CLOSE.\n");
+			break;
+
+		case IRP_MJ_SHUTDOWN:
+			DbgPrint("[$ARK] IRP_MJ_SHUTDOWN.\n");
+			break;
+
+		case IRP_MJ_DEVICE_CONTROL:
+			if (IOCTL_TRANSFER_TYPE(ioControlCode) == METHOD_NEITHER)
+			{
+				DbgPrint("[$ARK] METHOD_NEITHER\n");
+				outputBuffer = Irp->UserBuffer;
+			}
+
+			//
+			DbgPrint("[$XTR] IRP_MJ_DEVICE_CONTROL->IrpMjXTRdevCtrlRoutine(DeviceObject=0x%08x, Irp=0x%08x)->ARKioControl().\n", DeviceObject, Irp);
+
+			break;
 		}
 
 		//
-		DbgPrint("[$XTR] IRP_MJ_DEVICE_CONTROL->IrpMjXTRdevCtrlRoutine(DeviceObject=0x%08x, Irp=0x%08x)->ARKioControl().\n", DeviceObject, Irp);
- 
-		break;
+		// TODO: if not pending, call IoCompleteRequest and Irp is freed.
+		//
+
+		Irp->IoStatus.Status = ioStatus->Status;
+		Irp->IoStatus.Information = ioStatus->Information;
+		IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+		return STATUS_SUCCESS;
+	}
+	// A driver entry point
+	_Use_decl_annotations_ NTSTATUS DriverEntry(PDRIVER_OBJECT driver_object,
+		PUNICODE_STRING registry_path) {
+		/*	BOOLEAN symbolicLink;
+		UNICODE_STRING		ntDeviceName;
+		UNICODE_STRING		dosDeviceName;
+		PDEVICE_OBJECT		deviceObject = NULL;
+		PDEVICE_EXTENSION		deviceExtension = NULL;
+		*/
+
+		UNREFERENCED_PARAMETER(registry_path);
+		PAGED_CODE();
+		HYPERPLATFORM_LOG_DEBUG("start nested test \r\n");
+
+		static const wchar_t kLogFilePath[] = L"\\SystemRoot\\DdiMon33.log";
+
+		static const auto kLogLevel =
+			(IsReleaseBuild()) ? kLogPutLevelInfo | kLogOptDisableFunctionName
+			: kLogPutLevelDebug | kLogOptDisableFunctionName;
+
+		auto status = STATUS_UNSUCCESSFUL;
+
+		driver_object->DriverUnload = DriverpDriverUnload;
+
+
+		//HYPERPLATFORM_COMMON_DBG_BREAK();
+
+		// Request NX Non-Paged Pool when available
+		ExInitializeDriverRuntime(DrvRtPoolNxOptIn);
+
+		// Initialize log functions
+		bool need_reinitialization = false;
+		status = LogInitialization(kLogLevel, kLogFilePath);
+		if (status == STATUS_REINITIALIZATION_NEEDED) {
+			need_reinitialization = true;
+		}
+		else if (!NT_SUCCESS(status)) {
+			return status;
+		}
+
+		// Test if the system is supported
+		if (!DriverpIsSuppoetedOS()) {
+			LogTermination();
+			return STATUS_CANCELLED;
+		}
+
+		// Initialize perf functions
+		status = PerfInitialization();
+		if (!NT_SUCCESS(status)) {
+			LogTermination();
+			return status;
+		}
+		// Initialize utility functions 
+
+		status = UtilInitialization(driver_object);
+		if (!NT_SUCCESS(status)) {
+			//PerfTermination();
+			// LogTermination();
+			HYPERPLATFORM_LOG_DEBUG("UtilInitialization FAILED\r\n");
+			return status;
+		}
+
+		// Virtualize all processors
+
+		HYPERPLATFORM_LOG_INFO("Start VM IRQL: %x \r\n", KeGetCurrentIrql());
+		status = VmInitialization();
+
+		if (!NT_SUCCESS(status))
+		{
+			UtilTermination();
+			PerfTermination();
+			LogTermination();
+			return status;
+		}
+
+		HYPERPLATFORM_LOG_INFO("break IRQL: %x \r\n", KeGetCurrentIrql());
+
+		HYPERPLATFORM_LOG_INFO("The VMM has been installed. IRQL: %x \r\n", KeGetCurrentIrql());
+		return status;
 	}
 
-	//
-	// TODO: if not pending, call IoCompleteRequest and Irp is freed.
-	//
+	// Unload handler
+	_Use_decl_annotations_ static void DriverpDriverUnload(
+		PDRIVER_OBJECT driver_object) {
+		UNREFERENCED_PARAMETER(driver_object);
+		PAGED_CODE();
 
-	Irp->IoStatus.Status = ioStatus->Status;
-	Irp->IoStatus.Information = ioStatus->Information;
-	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+		HYPERPLATFORM_COMMON_DBG_BREAK();
 
-	return STATUS_SUCCESS;
-} 
-// A driver entry point
-_Use_decl_annotations_ NTSTATUS DriverEntry(PDRIVER_OBJECT driver_object,
-                                            PUNICODE_STRING registry_path) {
-/*	BOOLEAN symbolicLink;
-	UNICODE_STRING		ntDeviceName;
-	UNICODE_STRING		dosDeviceName;
-	PDEVICE_OBJECT		deviceObject = NULL;
-	PDEVICE_EXTENSION		deviceExtension = NULL; 
-	*/
+		VmTermination();
+		UtilTermination();
+		PerfTermination();
+		LogTermination();
+	}
 
-  UNREFERENCED_PARAMETER(registry_path);
-  PAGED_CODE();
-  HYPERPLATFORM_LOG_DEBUG("start nested test \r\n");
+	// Test if the system is one of supported OS versions
+	_Use_decl_annotations_ bool DriverpIsSuppoetedOS() {
+		PAGED_CODE();
 
-  static const wchar_t kLogFilePath[] = L"\\SystemRoot\\DdiMon33.log";
-  
-  static const auto kLogLevel =
-      (IsReleaseBuild()) ? kLogPutLevelInfo | kLogOptDisableFunctionName
-                         : kLogPutLevelDebug | kLogOptDisableFunctionName;
- 
-  auto status = STATUS_UNSUCCESSFUL; 
-     
-  driver_object->DriverUnload = DriverpDriverUnload;
-
-  
-  //HYPERPLATFORM_COMMON_DBG_BREAK();
-
-  // Request NX Non-Paged Pool when available
-  ExInitializeDriverRuntime(DrvRtPoolNxOptIn);
-  
-  // Initialize log functions
-  bool need_reinitialization = false;
-  status = LogInitialization(kLogLevel, kLogFilePath);
-  if (status == STATUS_REINITIALIZATION_NEEDED) {
-    need_reinitialization = true;
-  } else if (!NT_SUCCESS(status)) {
-    return status;
-  }
- 
-  // Test if the system is supported
-  if (!DriverpIsSuppoetedOS()) {
-    LogTermination();
-    return STATUS_CANCELLED;
-  }
-
-  // Initialize perf functions
-  status = PerfInitialization();
-  if (!NT_SUCCESS(status)) {
-    LogTermination();
-    return status;
-  } 
-  // Initialize utility functions 
-
-  status = UtilInitialization(driver_object);
-  if (!NT_SUCCESS(status)) {
-    //PerfTermination();
-   // LogTermination();
-	  HYPERPLATFORM_LOG_DEBUG("UtilInitialization FAILED\r\n");
-    return status;
-  } 
-
-  // Virtualize all processors
-
-  HYPERPLATFORM_LOG_INFO("Start VM IRQL: %x \r\n", KeGetCurrentIrql());
-  status = VmInitialization();
- 
-  if (!NT_SUCCESS(status))
-  {
-     UtilTermination();
-     PerfTermination();
-    LogTermination();	
-    return status;
-  } 
-
-  HYPERPLATFORM_LOG_INFO("break IRQL: %x \r\n", KeGetCurrentIrql()); 
-   
-  HYPERPLATFORM_LOG_INFO("The VMM has been installed. IRQL: %x \r\n", KeGetCurrentIrql());
-  return status;
-}
-
-// Unload handler
-_Use_decl_annotations_ static void DriverpDriverUnload(
-    PDRIVER_OBJECT driver_object) {
-  UNREFERENCED_PARAMETER(driver_object);
-  PAGED_CODE();
-
-  HYPERPLATFORM_COMMON_DBG_BREAK();
-
-  VmTermination();
-  UtilTermination();
-  PerfTermination();
-  LogTermination();
-}
-
-// Test if the system is one of supported OS versions
-_Use_decl_annotations_ bool DriverpIsSuppoetedOS() {
-  PAGED_CODE();
-
-  RTL_OSVERSIONINFOW os_version = {};
-  auto status = RtlGetVersion(&os_version);
-  if (!NT_SUCCESS(status)) {
-    return false;
-  }
-  if (os_version.dwMajorVersion != 6 && os_version.dwMajorVersion != 10) {
-    return false;
-  }
-  // 4-gigabyte tuning (4GT) should not be enabled
-  if (!IsX64() &&
-      reinterpret_cast<ULONG_PTR>(MmSystemRangeStart) != 0x80000000) {
-    return false;
-  }
-  return true;
-}
+		RTL_OSVERSIONINFOW os_version = {};
+		auto status = RtlGetVersion(&os_version);
+		if (!NT_SUCCESS(status)) {
+			return false;
+		}
+		if (os_version.dwMajorVersion != 6 && os_version.dwMajorVersion != 10) {
+			return false;
+		}
+		// 4-gigabyte tuning (4GT) should not be enabled
+		if (!IsX64() &&
+			reinterpret_cast<ULONG_PTR>(MmSystemRangeStart) != 0x80000000) {
+			return false;
+		}
+		return true;
+	}
 
 }  // extern "C"
 
